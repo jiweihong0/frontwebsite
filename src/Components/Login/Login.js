18530bc6cb26a72d8e610c4078be89d3b16de1fe
XIN <ritashao0512@gmail.com>
2022-05-30 13:04:54 +0800

    xin

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,24 +2,45 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: "" };
+    this.changState = this.changState.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  changState(e) {
+    this.setState({ email: e.target.value });
+    console.log("OK");
+  }
+
+  submitForm(e) {
+    console.log("email");
+    e.preventDefault();
+  }
+
   render() {
+    const { email } = this.state;
+    const { changState, submitForm } = this;
     return (
-      <form>
+      <form onSubmit={submitForm}>
         <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
+          <label for="Email">Email address</label>
           <input
             type="email"
             class="form-control"
-            id="exampleInputEmail1"
+            id="Email"
             aria-describedby="emailHelp"
             placeholder="Enter email"
+            value={email}
+            onChange={changState}
           />
-          <p>
-            <small id="emailHelp" class="form-text text-muted">
-              We'll never share your email with anyone else.
-            </small>
-          </p>
+          <br />
+          <small id="emailHelp" class="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
         </div>
+        <br />
         <button type="submit" class="btn btn-primary">
           Submit
         </button>
